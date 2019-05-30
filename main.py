e7b8f4144c19e32881e43592941c5ba5eca58b98
cristophervidalmachado <cristophervidal3@gmail.com>
2019-05-30 14:48:55 -0300

    Creating a function to showVertices and showPoints

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ class Wireframe:
         ones_column = np.ones((len(node_array), 1))
         ones_added = np.hstack((node_array, ones_column))
         self.points = np.vstack((self.points, ones_added))
+    
+    def addVertices(self, verticeList):
+        self.vertices += verticeList
+    
+    def showVertices(self):
+        print("\n Vertices:")
+        for i, (node1, node2) in enumerate(self.vertices):
+            print("   %d: %d -> %d" % (i, node1, node2))
+
+    def showPoints(self):
+        print("\n Points:")
+        print("  [P]: ( x, y, z)")
+        for i, (x, y, z, _) in enumerate(self.points):
+            print("   %d: (%d, %d, %d)" % (i, x, y, z))
+            
 
 def cubeCreateInCenter(centerX, centerY, centerZ, sideLength):
         
@@ -34,4 +49,10 @@ if __name__ == "__main__":
     cube = Wireframe()
     cube_points = cubeCreateInCenter(resolutionX/2,resolutionY/2,0,100)
     cube.addPoints(cube_points)
+    cube.addVertices([(n,n+4) for n in range(0,4)])
+    cube.addVertices([(n,n+1) for n in range(0,8,2)])
+    cube.addVertices([(n,n+2) for n in (0,1,4,5)])
+    cube.showPoints()
+    cube.showVertices()
+    
     print(cube_points)
